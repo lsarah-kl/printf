ef83aa074e72fa886c183289b64f6538ae91886d
Sarah <sarah.t.karim@gmail.com>
2023-05-28 14:04:53 -0700

    2

--- main.h
+++ main.h
@@ -2,7 +2,15 @@
 #define MAIN_H
 
 #include <stdarg.h>
+#include <unistd.h>
 
+#define UNUSED(x) (void)(x)
+
+/* Function protoypes */
 int _printf(const char *format, ...);
+int print_char(va_list args);
+int print_string(va_list args);
+int print_binary(va_list args);
+int print_percent(va_list args);
 
 #endif /* MAIN_H */
